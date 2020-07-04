484c398a81a1b345792165c4ea20281f2fe0b3ec
Mario Ochoa <mario8ato@hotmail.com>
2020-07-03 20:44:12 -0500

    Creando el state y validando formulario de crear producto

--- src/actions/productoActions.js
+++ src/actions/productoActions.js
@@ -1,29 +1,22 @@
- 
 import {
     AGREGAR_PRODUCTO,
     AGREGAR_PRODUCTO_EXITO,
     AGREGAR_PRODUCTO_ERROR,
     // COMENZAR_DESCARGA_PRODUCTOS,
     // DESCARGA_PRODUCTOS_EXITO,
-    // DESCARGA_PRODUCTOS_ERROR,
+    // DESCARGA_PRODUCTOS_ERROR, 
     // OBTENER_PRODUCTO_ELIMINAR,
     // PRODUCTO_ELIMINADO_EXITO,
     // PRODUCTO_ELIMINADO_ERROR,
     // OBTENER_PRODUCTO_EDITAR,
+    // COMENZAR_EDICION_PRODUCTO,
     // PRODUCTO_EDITADO_EXITO,
     // PRODUCTO_EDITADO_ERROR
 } from '../types';
 
-// cada reducer tiene su propio state
-const initialState = {
-    productos: [],
-    error: null,
-    loading: false, 
-}
-
-export default function(state = initialState, action) {
-    switch (action.type) {
-        default:
-            return state;
+//crear nuevos productos
+export function crearNuevoProductoAction(producto) {
+    return () => {
+        
     }
 }
