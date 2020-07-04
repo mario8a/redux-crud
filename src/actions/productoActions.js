23acd72518300c20376d3c2954b1c5f48ca809ce
Mario Ochoa <mario8ato@hotmail.com>
2020-07-04 00:06:15 -0500

    Eliminando producto, alerta de confirmacion para eliminar producto

--- src/actions/productoActions.js
+++ src/actions/productoActions.js
@@ -2,12 +2,12 @@ import {
     AGREGAR_PRODUCTO,
     AGREGAR_PRODUCTO_EXITO,
     AGREGAR_PRODUCTO_ERROR,
-    // COMENZAR_DESCARGA_PRODUCTOS,
-    // DESCARGA_PRODUCTOS_EXITO,
-    // DESCARGA_PRODUCTOS_ERROR, 
-    // OBTENER_PRODUCTO_ELIMINAR,
-    // PRODUCTO_ELIMINADO_EXITO,
-    // PRODUCTO_ELIMINADO_ERROR,
+    COMENZAR_DESCARGA_PRODUCTOS,
+    DESCARGA_PRODUCTOS_EXITO,
+    DESCARGA_PRODUCTOS_ERROR, 
+    OBTENER_PRODUCTO_ELIMINAR,
+    PRODUCTO_ELIMINADO_EXITO,
+    PRODUCTO_ELIMINADO_ERROR,
     // OBTENER_PRODUCTO_EDITAR,
     // COMENZAR_EDICION_PRODUCTO,
     // PRODUCTO_EDITADO_EXITO,
@@ -66,3 +66,69 @@ const agregarProductoError = estado => ({
     type: AGREGAR_PRODUCTO_ERROR,
     payload: estado
 });
+
+//funcion que desarga los productos de la db
+export function obtenerProductosAction() {
+    return async (dispatch) => {
+        dispatch(descargarProductos());
+
+        try {
+            const respuesta = await clienteAxios.get('/productos');
+            dispatch(descargaProductosExitosa(respuesta.data));
+        } catch (error) {
+            console.log(error);
+            dispatch(descargarProductosError())
+        }
+    }
+}
+
+const descargarProductos = () => ({
+    type: COMENZAR_DESCARGA_PRODUCTOS,
+    payload: true
+})
+
+const descargaProductosExitosa = productos => ({
+    type: DESCARGA_PRODUCTOS_EXITO,
+    payload: productos
+})
+
+const descargarProductosError = () => ({
+    type: DESCARGA_PRODUCTOS_ERROR,
+    payload: true
+})
+
+//Selecciona y elimina el producto
+export function borrarProductoAction(id) {
+    return async (dispatch) => {
+        dispatch(obtenerProductoEliminar(id))
+
+        try {
+            await clienteAxios.delete(`/productos/${id}`);
+            dispatch(eliminarProductoExito())
+
+            //si se elimina, mostrar alerta
+            Swal.fire(
+                'Eliminado!',
+                'El producto se elimino correctamente',
+                'success'
+              )
+        } catch (error) {
+            console.log(error);
+            dispatch(eliminarProductoError())
+        }
+    }
+}
+
+const obtenerProductoEliminar = id => ({
+    type: OBTENER_PRODUCTO_ELIMINAR,
+    payload: id
+});
+
+const eliminarProductoExito = () => ({
+    type: PRODUCTO_ELIMINADO_EXITO
+});
+
+const eliminarProductoError = () => ({
+    type: PRODUCTO_ELIMINADO_ERROR,
+    payload: true
+})
--- src/reducers/productosReducer.js
+++ src/reducers/productosReducer.js
@@ -3,12 +3,12 @@ import {
     AGREGAR_PRODUCTO,
     AGREGAR_PRODUCTO_EXITO,
     AGREGAR_PRODUCTO_ERROR,
-    // COMENZAR_DESCARGA_PRODUCTOS,
-    // DESCARGA_PRODUCTOS_EXITO,
-    // DESCARGA_PRODUCTOS_ERROR,
-    // OBTENER_PRODUCTO_ELIMINAR,
-    // PRODUCTO_ELIMINADO_EXITO,
-    // PRODUCTO_ELIMINADO_ERROR,
+    COMENZAR_DESCARGA_PRODUCTOS,
+    DESCARGA_PRODUCTOS_EXITO,
+    DESCARGA_PRODUCTOS_ERROR,
+    OBTENER_PRODUCTO_ELIMINAR,
+    PRODUCTO_ELIMINADO_EXITO,
+    PRODUCTO_ELIMINADO_ERROR,
     // OBTENER_PRODUCTO_EDITAR,
     // PRODUCTO_EDITADO_EXITO,
     // PRODUCTO_EDITADO_ERROR
@@ -18,11 +18,13 @@ import {
 const initialState = {
     productos: [],
     error: null,
-    loading: false, 
+    loading: false,
+    productoeliminar: null
 }
 
 export default function(state = initialState, action) {
     switch (action.type) {
+        case COMENZAR_DESCARGA_PRODUCTOS: 
         case AGREGAR_PRODUCTO: 
             return {
                 ...state,
@@ -35,10 +37,30 @@ export default function(state = initialState, action) {
                 productos: [...state.productos, action.payload]
             }
         case AGREGAR_PRODUCTO_ERROR:
+        case DESCARGA_PRODUCTOS_ERROR:
+        case PRODUCTO_ELIMINADO_ERROR:
             return {
                 ...state,
                 loading: false,
                 error: action.payload
+            }
+        case DESCARGA_PRODUCTOS_EXITO:
+            return {
+                ...state,
+                loading: false,
+                error: null,
+                productos: action.payload
+            }
+        case OBTENER_PRODUCTO_ELIMINAR: 
+            return {
+                ...state,
+                productoeliminar: action.payload
+            }
+        case PRODUCTO_ELIMINADO_EXITO:
+            return {
+                ...state,
+                productos: state.productos.filter(producto => producto.id !== state.productoeliminar ),
+                productoeliminar: null
             }
         default:
             return state;
